67c6881a892df2eeab0eb02c2ce825e45260b073
Henri Schmidt <henri@reschmi.de>
2016-10-19 21:42:02 +0200

    Tank and Projectile now inherit from Entity

--- src/main/kotlin/de/randomerror/genetictank/entities/Entity.kt
+++ src/main/kotlin/de/randomerror/genetictank/entities/Entity.kt
@@ -1,6 +1,18 @@
 package de.randomerror.genetictank.entities
 
+import javafx.scene.canvas.GraphicsContext
+
 /**
  * Created by henri on 19.10.16.
  */
 
+abstract class Entity {
+    var x = 0
+    var y = 0
+
+    var velX = 0.0
+    var velY = 0.0
+
+    abstract fun render(gc: GraphicsContext)
+    abstract fun update(deltaTime: Double)
+}
--- src/main/kotlin/de/randomerror/genetictank/entities/Tank.kt
+++ src/main/kotlin/de/randomerror/genetictank/entities/Tank.kt
@@ -1,5 +1,20 @@
 package de.randomerror.genetictank.entities
 
+import javafx.scene.canvas.GraphicsContext
+
 /**
  * Created by henri on 19.10.16.
  */
+
+class Tank : Entity() {
+
+
+    override fun render(gc: GraphicsContext) {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun update(deltaTime: Double) {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
