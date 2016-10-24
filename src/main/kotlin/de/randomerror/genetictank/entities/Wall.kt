92ae5348b40bcb7bebf2be696c22a18d575c6bed
Jannis Kaiser <jannis.kaiser2@gmail.com>
2016-10-24 02:09:32 +0200

    create Wall

---
+++ src/main/kotlin/de/randomerror/genetictank/entities/Wall.kt
@@ -0,0 +1,26 @@
+package de.randomerror.genetictank.entities
+
+import javafx.scene.canvas.GraphicsContext
+import javafx.scene.shape.StrokeLineJoin.ROUND
+
+/**
+ * Created by Jannis on 24.10.16.
+ */
+class Wall(x: Double, y: Double, val w: Double, val h: Double) : Entity() {
+    
+    init {
+        this.x = x
+        this.y = y
+    }
+
+    override fun render(gc: GraphicsContext) = gc.run {
+        lineWidth = 3.0
+        lineJoin = ROUND
+
+        fillRect(x, y, w, h)
+    }
+    
+    override fun update(deltaTime: Double) {
+        
+    }
+}
