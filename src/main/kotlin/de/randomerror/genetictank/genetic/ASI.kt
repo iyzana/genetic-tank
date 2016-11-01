69df53c99d9ef92794cb7b2839efe5f09aabb7d8
Jannis Kaiser <jannis.kaiser2@gmail.com>
2016-11-01 02:18:46 +0100

    simplify Matrix initialization

--- src/main/kotlin/de/randomerror/genetictank/genetic/ASI.kt
+++ src/main/kotlin/de/randomerror/genetictank/genetic/ASI.kt
@@ -1,7 +1,6 @@
 package de.randomerror.genetictank.genetic
 
 import de.randomerror.genetictank.helper.Matrix
-import java.util.*
 
 /**
  * Created by henri on 24.10.16.
@@ -12,8 +11,8 @@ class ASI(val layers: List<Int>) {
     val theNetwork: List<Pair<Matrix, Matrix>>
 
     init {
-        theNetwork = (0 until layers.size-1).map { i->
-            Matrix.random(1, layers[i]) to Matrix.random(layers[i], layers[i+1])
+        theNetwork = (0 until layers.size - 1).map { i ->
+            Matrix.random(1, layers[i]) to Matrix.random(layers[i], layers[i + 1])
         }
     }
 
@@ -27,12 +26,8 @@ class ASI(val layers: List<Int>) {
     private fun sigmoid(vector: Matrix): Matrix {
         require(vector.x == 1)
 
-        val result = Matrix(1, vector.y)
-
-        vector.data[0].forEachIndexed { i, d ->
-            result.data[0][i] = 1.0/(1.0+Math.exp(-d))
+        return Matrix(1, vector.y) { i, j ->
+            1.0 / (1.0 + Math.exp(-vector.data[i][j]))
         }
-
-        return result
     }
 }
--- src/main/kotlin/de/randomerror/genetictank/helper/Matrix.kt
+++ src/main/kotlin/de/randomerror/genetictank/helper/Matrix.kt
@@ -5,44 +5,30 @@ import java.util.*
 /**
  * Created by henri on 01.11.16.
  */
-data class Matrix(val x: Int, val y: Int, val data: Array<Array<Double>> = Array(x) { Array(y) {0.0} }) {
-
+class Matrix(val x: Int, val y: Int, init: (i: Int, j: Int) -> Double) {
+    val data: Array<Array<Double>> = Array(x) { i -> Array(y) { j -> init(i, j) } }
 
     operator fun times(other: Matrix): Matrix {
         require(x == other.y)
 
-        val m = Matrix(other.x, y)
-
-        (0 until other.x).forEach { i ->
-            (0 until y).forEach { j ->
-                m.data[i][j] = (0 until x).sumByDouble { data[it][j]*other.data[i][it] }
-            }
+        return Matrix(other.x, y) { i, j ->
+            (0 until x).sumByDouble { data[it][j] * other.data[i][it] }
         }
-
-        return m
     }
 
     operator fun plus(other: Matrix): Matrix {
         require(x == other.x && y == other.y)
 
-        val m = Matrix(x, y)
-        data.forEachIndexed { i, columns ->
-            columns.forEachIndexed { j, d ->
-                m.data[i][j] = d+other.data[i][j]
-            }
+        return Matrix(x, y) { i, j ->
+            data[i][j] + other.data[i][j]
         }
-        return m
     }
 
     companion object {
         fun random(x: Int, y: Int, range: ClosedRange<Double> = -1.0..1.0): Matrix {
-            val m = Matrix(x, y)
-            (0 until x).forEach { i ->
-                (0 until y).forEach { j ->
-                    m.data[i][j] = Math.random()*(range.endInclusive-range.start)+range.start
-                }
+            return Matrix(x, y) { i, j ->
+                Math.random() * (range.endInclusive - range.start) + range.start
             }
-            return m
         }
     }
 
